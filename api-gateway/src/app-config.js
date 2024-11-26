553d3d88906d6e9a7df44cc700ed1a7c2ab4ff78
Harshit Kumar <83143382+HarshitKumar1124@users.noreply.github.com>
2024-11-27 04:57:10 +0530

    Update app-config.js

--- api-gateway/src/app-config.js
+++ api-gateway/src/app-config.js
@@ -14,7 +14,7 @@ module.exports = async(app) =>{
         app.get('/',async(req,res)=>{
             res.status(200).send({
                 status:true,
-                message:`API Gateway is live at port :: ${process.env.PORT}` + `CI-CD Implemented Successfully.`
+                message:`API Gateway is live at port :: ${process.env.PORT}` + ` CI-CD Implemented Successfully.`
             });
         })
         
